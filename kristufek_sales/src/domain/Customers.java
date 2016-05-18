e64d6b4784717457eca108f57992aebc12e552fc
korksi-spengergasse <kri17201@spengergasse.at>
2016-05-18 19:36:48 +0200

    Add property to Customers

--- kristufek_sales/src/domain/Customers.java
+++ kristufek_sales/src/domain/Customers.java
@@ -1,5 +1,17 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class Customers {
 
+		private ArrayList<Customer> customer;
+
+		Customers() {
+			this.customer = new ArrayList<Customer>();
+		}
+
+		public ArrayList<Customer> getCustomer() {
+			return customer;
+		}
+	
 }
